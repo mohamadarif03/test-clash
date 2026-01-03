558eb1feeea062d5c86b1070c961663073e3b927
mohamadarif03 <mohamadarif5392@gmail.com>
2026-01-03 23:59:33 +0700

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"test-clash-be/internal/router"
+	"test-clash-be/pkg/database"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: No .env file found")
+	}
+
+	database.Connect()
+
+	r := router.SetupRouter()
+
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
+}
